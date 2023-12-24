4e8881c57eaa5c06ef644c8e42428478826b95bb
baiych <baiych@shanghaitech.edu.cn>
2023-12-24 23:37:44 +0800

    doc + some new syscall

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -11,4 +11,12 @@ void syscall_init (void);
 bool user_has_access (uint8_t __user *, size_t);
 bool kernel_has_access (uint8_t __user *, size_t);
 
+#ifdef FILESYS
+bool chdir (const char *dir);
+bool mkdir (const char *dir);
+bool readdir (int fd, char *name);
+bool isdir (int fd);
+int inumber (int fd);
+#endif
+
 #endif /* userprog/syscall.h */
